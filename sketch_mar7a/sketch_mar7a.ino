29a6a7c62020e4d8d65247c08237f690390e9b58
RhynJ <87622045+RhynJ@users.noreply.github.com>
2023-04-21 11:28:15 +0100

    this should work with the PWM
    I have not tested to find out

--- sketch_mar7a/sketch_mar7a.ino
+++ sketch_mar7a/sketch_mar7a.ino
@@ -1,4 +1,12 @@
 #include <Servo.h>
+#include <Adafruit_PWMServoDriver.h>
+#include <Wire.h>
+
+Adafruit_PWMServoDriver PWM = Adafruit_PWMServoDriver();
+
+
+
+
 
 Servo lowerMot;
 int lowerPot = A0;
@@ -8,6 +16,8 @@ Servo upperMot;
 int upperPot = A3;
 int upperVal;
 
+int servo;
+
 
 void setup() {
   // put your setup code here, to run once:
@@ -16,16 +26,35 @@ void setup() {
   
   lowerMot.attach(0);
   pinMode(lowerPot, INPUT);
+
+  Serial.begin(9600); //buad
+  PWM.begin(); 
+  PWM.setPWMFreq(60);  //frequency
 }
 
+
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  //this gathers the pots data and converts it to digital
   lowerVal = analogRead(lowerPot);
   lowerVal = map(lowerPot,0,1028,0,180);
-  lowerMot.write(lowerVal);
+  servo = 1;  //this will tell the function whixh servo to use
+  PWM.setPWM(servo, 0, lowerVal); //this will tell the PWM which servo to use, and the value it wants the motor to spin to
+
+
+
 
   upperVal = analogRead(upperPot);
   upperVal = map(lowerPot, 0, 1028, 0, 180);
-  upperMot.write(lowerPot);
+  servo =2;
+  PWM.setPWM(servo, 0, upperVal);
 
 }
+
+
+
+
+
+
+
+
