729b5170e8946739d21a196d25462617c7c0762c
RhynJ <87622045+RhynJ@users.noreply.github.com>
2023-04-22 13:18:03 +0100

    the motor now spins when the pot moves
    there may need to be some calibration but everything is working

--- sketch_mar7a/sketch_mar7a.ino
+++ sketch_mar7a/sketch_mar7a.ino
@@ -1,57 +1,41 @@
-#include <Servo.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <Wire.h>
 
-Adafruit_PWMServoDriver PWM = Adafruit_PWMServoDriver();
+Adafruit_PWMServoDriver motorPWM = Adafruit_PWMServoDriver();
 
 
 
-
-
-Servo lowerMot;
-int lowerPot = A0;
-int lowerVal;
-
-Servo upperMot;
-int upperPot = A3;
+int upperPot = A1;
 int upperVal;
-
 int servo;
+int the;
 
 
 void setup() {
   // put your setup code here, to run once:
-  upperMot.attach(3);
-  pinMode(upperPot, INPUT); 
-  
-  lowerMot.attach(0);
-  pinMode(lowerPot, INPUT);
-
-  Serial.begin(9600); //buad
-  PWM.begin(); 
-  PWM.setPWMFreq(60);  //frequency
+  pinMode(upperPot, INPUT);   
+  Serial.begin(9600);
+  motorPWM.begin();
+  motorPWM.setPWMFreq(60);
 }
 
 
 void loop() {
 
   //this gathers the pots data and converts it to digital
-  lowerVal = analogRead(lowerPot);
-  lowerVal = map(lowerPot,0,1028,0,180);
+  float lowerVal = analogRead(A0);
+  servo = 0;  //this will tell the function whixh servo to use
+  lowerVal = lowerVal / 2;
+  motorPWM.setPWM(servo, 0, lowerVal); //this will tell the PWM which servo to use, and the value it wants the motor to spin to    
+  
+
+    //this gathers the pots data and converts it to digital
+  float upperVal = analogRead(A1);
   servo = 1;  //this will tell the function whixh servo to use
-  PWM.setPWM(servo, 0, lowerVal); //this will tell the PWM which servo to use, and the value it wants the motor to spin to
-
-
-
-
-  upperVal = analogRead(upperPot);
-  upperVal = map(lowerPot, 0, 1028, 0, 180);
-  servo =2;
-  PWM.setPWM(servo, 0, upperVal);
+  upperVal = upperVal / 2;
+  motorPWM.setPWM(servo, 0, upperVal); //this will tell the PWM which servo to use, and the value it wants the motor to spin to    
 
 }
-
-
 
 
 
