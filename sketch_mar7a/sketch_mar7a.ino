7037589a971b958504cd5a91a1ef19e02f0bea6d
RhynJ <87622045+RhynJ@users.noreply.github.com>
2023-05-08 19:06:06 +0100

    .

--- sketch_mar7a/sketch_mar7a.ino
+++ sketch_mar7a/sketch_mar7a.ino
@@ -11,7 +11,7 @@ int servo;
 
 void setup() {
   // put your setup code here, to run once:
-  pinMode(upperPot, INPUT);   
+  //  pinMode(upperPot, INPUT);
   Serial.begin(9600);
   motorPWM.begin();
   motorPWM.setPWMFreq(60);
@@ -23,20 +23,14 @@ void loop() {
   //this gathers the pots data and converts it to digital
   float lowerVal = analogRead(A0);
   servo = 0;  //this will tell the function whixh servo to use
-  lowerVal = lowerVal / 2;
-  motorPWM.setPWM(servo, 0, lowerVal); //this will tell the PWM which servo to use, and the value it wants the motor to spin to    
+  lowerVal = lowerVal;
+  motorPWM.setPWM(servo, 0, lowerVal);  //this will tell the PWM which servo to use, and the value it wants the motor to spin to
   
 
-    //this gathers the pots data and converts it to digital
-  float upperVal = analogRead(A1);
+
+  //this gathers the pots data and converts it to digital
+  float upperVal = analogRead(A2);
   servo = 1;  //this will tell the function whixh servo to use
-  upperVal = upperVal / 2;
-  motorPWM.setPWM(servo, 0, upperVal); //this will tell the PWM which servo to use, and the value it wants the motor to spin to    
-
+  upperVal = upperVal;
+  motorPWM.setPWM(servo, 0, upperVal);  //this will tell the PWM which servo to use, and the value it wants the motor to spin to
 }
-
-
-
-
-
-
